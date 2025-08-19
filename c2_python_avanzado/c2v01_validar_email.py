b78e626106f84af5fae251c68c12779c3c232120
LourdesLozanoGS <169046653+LourdesLozanoGS@users.noreply.github.com>
2025-08-19 11:30:52 +0000

    modif

--- c2_python_avanzado/c2v01_validar_email.py
+++ c2_python_avanzado/c2v01_validar_email.py
@@ -3,10 +3,15 @@ import re
 
 def email_valido(email):
 
+    # se usa "r" para que no tome \ como escape
+    # a-z: calquier caracter entre la "a" y la "z". 0-9: indica cualquier digito. 
+    # el "+" indicia que peude haber mas d eun caracter de este tipo
+    # el * busc auna o mas ocurrecnia dentro del nombre de usuario
     formato_valido = r"^([a-z0-9]+[.-_])*[a-z0-9]+@[a-z0-9-]+(\.[a-z]{2,})+$"
+    # IGNORECASE hace que no distinga entre mayusculas y minusuclas
     if re.match(formato_valido, email, re.IGNORECASE):
         return True
     return False
 
-valido = email_valido("anaPPPP@email.edu.co")
+valido = email_valido("lourLM@email.co")
 print("Email valido:", valido)
